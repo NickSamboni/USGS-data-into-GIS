23608b98bd406b529dc5e6c2995c91dd5ee283d9
Nicolas Ramos <ramossamboni@gmail.com>
2021-10-21 12:30:58 +0200

    adding coments to the scripts

--- mongoconnection.py
+++ mongoconnection.py
@@ -1,20 +1,24 @@
 import pymongo as mongo
-from scraper import getjson
+from scraper import getdata
 
-data = getjson()
+#getting the returned object from the function that constructs the data 
+data = getdata()
 print("Function Working Properly")
 
+#connecting to mongodb local database
 try:
     connection = mongo.MongoClient('mongodb://localhost:27017/')
     print("Connected successfully!!!")
 except:
     print("Could not connect to MongoDB")
 
+#these are the credentials for the specific database and collection of choise
 db = connection["USGS"]
 Collection = db["testing"]
 
+#checking for the structure of the object returned from getdata()
 if isinstance(data, list):
-    Collection.drop()
+    #Collection.drop()
     Collection.insert_many(data)
 else:
     Collection.drop()
--- scraper.py
+++ scraper.py
@@ -2,16 +2,21 @@ import pandas as pd
 import re
 
 
-def getjson():
+def getdata():
+    #url that points to the csv file that contains all the data about earthquakes around the world
     url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv'
+
+    #using pandas to work with the dataframe
     raw_data = pd.read_csv(url)
 
+    #create the objs that will work as columns for the final df
     time = raw_data['time']
     lat = raw_data['latitude']
     lon = raw_data['longitude']
     depth = raw_data['depth']
     mag = raw_data['mag']
 
+    #forming the final df. Creating two new columns with the geo information.
     zone, place = [], []
     for row in raw_data['place']:
         x = re.split(',', row)
@@ -30,14 +35,20 @@ def getjson():
         }
     )
 
+    #Creating the geojson object neccesary to send data to mongodb as different documents
+    #because it is needed to satisfy a specific structure
     mongo_docs = []
     for _, row in final_data.iterrows():
         feature = {'type': 'Feature',
                    'properties': {},
                    'geometry': {'type': 'Point',
-                                'coordinates': []}}
+                                'coordinates': []
+                                }
+                    }
         feature['geometry']['coordinates'] = [row['longitude'], row['latitude']]
         for prop in final_data.columns:
             feature['properties'][prop] = row[prop]
         mongo_docs.append(feature)
+
+    #returning a list of dictionaries that contain the information for each seismic event
     return mongo_docs
